d0a96485cb8e79e0267143eef1f4a463dc02f52d
jarss15447-spec <jarss15447@gmail.com>
2025-12-05 16:55:54 -0600

    Primera version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '40px' }}>
+      <h1>Página de prueba en React</h1>
+      <p>Este es un sitio simple para aprender despliegue y actualización.</p>
+    </div>
+  );
+}
+
+export default App;
